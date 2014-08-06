604ce8f2b1f8448c5a060cf97a4e26dcdb5e4913
Ubuntu <ubuntu@ip-10-75-23-138.eu-west-1.compute.internal>
2014-08-06 15:29:24 +0000

    jshint cleanup

--- analyze.js
+++ analyze.js
@@ -1,31 +1,41 @@
 #!/usr/bin/env node
+/*
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-var fs          = require('fs')
-  , configFile  = process.argv[2]
-  , analyze     = require('./')
-  , AWS         = require('aws-sdk')
-  , config
+'use strict';
+
+
+
+var fs = require('fs');
+var configFile = process.argv[2];
+var analyze = require('./');
+var AWS = require('aws-sdk');
+var config;
 
 if (!configFile) {
-  console.log('Missing config')
-  process.exit(-1)
+  console.log('Missing config');
+  process.exit(-1);
 }
 
-try {
-  AWS.config.loadFromPath(configFile)
-  config = JSON.parse(fs.readFileSync(configFile))
-} catch(err) {
-  console.log(err)
-  console.log('unable to read', configFile)
-  process.exit(-1)
-}
 
+AWS.config.loadFromPath(configFile)
+config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+
+
+console.log('Analyzing...');
 analyze(config, function(err, status) {
-  if (err) {
-    console.log(err)
-    return
-  }
-
-  console.log(JSON.stringify(status, null, 2))
-})
+  if (err) { console.log(err); }
+  console.log(JSON.stringify(status, null, 2));
+});
 
--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-var async = require('async')
-  , fetchInstances = require('./lib/ec2-instances')
-  , fetchImages = require('./lib/ec2-amis')
-  , fetchContainers = require('./lib/docker-containers')
-  , fetchLoadBalancers = require('./lib/elb')
-  , debug = require('debug')('aws-analyzer:index')
+/*
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+'use strict';
+
+var async = require('async');
+var fetchInstances = require('./lib/ec2-instances');
+var fetchImages = require('./lib/ec2-amis');
+var fetchContainers = require('./lib/docker-containers');
+var fetchLoadBalancers = require('./lib/elb');
 
 function analyze(config, cb) {
-  var result = {}
-
-  debug('started')
+  var result = {};
 
   async.eachSeries([
-      fetchInstances
-    , fetchImages
-    , fetchContainers
-    , fetchLoadBalancers
+    fetchInstances,
+    fetchImages,
+    fetchContainers
+//    fetchLoadBalancers
   ], function(func, cb) {
-    debug('calling', func.name)
     func(config, result, function(err) {
-      if (err) { return cb(err) }
-
-      debug('done', func.name)
-
-      cb(null)
-    })
-  }, function(err) {
-    cb(err, result)
-  })
+      if (err) { return cb(err); }
+      cb(null);
+    });
+  }, function(err) { cb(err, result); });
 }
 
-module.exports = analyze
+module.exports = analyze;
+
