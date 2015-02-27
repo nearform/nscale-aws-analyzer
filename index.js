a6339a50a1bf187f7b8c3ce2a3bd70fd7afc3d6b
Matteo Collina <hello@matteocollina.com>
2015-02-27 18:34:24 +0100

    Added canAnalyze function.

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ var stripExtraneous = require('./lib/stripExtraneous');
 var fetchLoadBalancers = require('./lib/elb');
 var postProcessing = require('./lib/postProcessing');
 var match = require('./lib/match');
+var allowedTypes = [
+  'docker',
+  'blank-container'
+];
 
 exports.analyze = function analyze(config, system, callback) {
   system = system || {
@@ -58,4 +62,23 @@ exports.analyze = function analyze(config, system, callback) {
 
     async.eachSeries(series, onNext, complete);
   });
+};
+
+/**
+ * Checks if this analyzer can analyze the given system.
+ * A direct-analyzer can analyze if it contains docker or process
+ * containers, and blank-containers with an IP address.
+ *
+ */
+exports.canAnalyze = function canAnalyze(system) {
+
+  var allAllowed = system.containerDefinitions.every(function(def) {
+    return allowedTypes.indexOf(def.type) >= 0 || def.type.indexOf('aws') === 0;
+  });
+
+  var isAWS = system.containerDefinitions.some(function(def) {
+    return def.type.indexOf('aws') === 0;
+  });
+
+  return allAllowed && isAWS;
 };
